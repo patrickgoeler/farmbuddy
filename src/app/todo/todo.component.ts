0cc66a3dc064898fbfcff6e2be8f394f89212ebf
Holger <holger@holgeralbrecht-4.local>
2019-11-23 23:12:49 +0100

    update

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+    selector: "app-todo",
+    templateUrl: "./todo.component.html",
+    styleUrls: ["./todo.component.scss"],
 })
 export class TodoComponent implements OnInit {
+    todos = [
+        {
+            title: "Blumenkübel kaufen",
+            checked: true,
+        },
+        {
+            title: "Samen kaufen",
+            checked: false,
+        },
+        {
+            title: "Schaufel kaufen",
+            checked: false,
+        },
+        {
+            title: "Erde kaufen",
+            checked: false,
+        },
+        {
+            title: "Gießkanne kaufen",
+            checked: true,
+        },
+    ]
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
