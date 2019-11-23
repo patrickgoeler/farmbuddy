a09975c5f62ffbbb2e6c5279f8803ce0b071d514
Holger <holger@holgeralbrecht-4.local>
2019-11-23 11:29:09 +0100

    intro comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core"
 import { Routes, RouterModule } from "@angular/router"
+import { IntroComponent } from "./intro/intro.component"
 
-const routes: Routes = []
+const routes: Routes = [
+    {
+        path: "intro",
+        component: IntroComponent,
+    },
+]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser"
+import { NgModule } from "@angular/core"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from "@nebular/theme"
+import { NbEvaIconsModule } from "@nebular/eva-icons"
+import { IntroComponent } from "./intro/intro.component"
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
-    NbLayoutModule,
-    NbEvaIconsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, IntroComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        NbThemeModule.forRoot({ name: "default" }),
+        NbLayoutModule,
+        NbButtonModule,
+        NbEvaIconsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
